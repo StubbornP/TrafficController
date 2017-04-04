ac17d344ca512f0623b07c09cdc4b074734bb8ff
stubborn <353804712@qq.com>
2017-04-03 23:10:11 -0700

    L2 info finished2

---
+++ html/js/index.js
@@ -0,0 +1,22 @@
+Callbacks = {
+
+    "Tolology_Clicked":function () {
+        $("#mainFrame").attr("src", "topology.html");
+        $('#mainFrame').attr('src', $('#mainFrame').attr('src'));
+    },
+    "L2Control_Clicked":function () {
+        $("#mainFrame").attr("src", "l2Control.html");
+        $('#mainFrame').attr('src', $('#mainFrame').attr('src'));
+    },
+    "L3Control_Clicked":function () {
+        $("#mainFrame").attr("src", "l3Control.html");
+        $('#mainFrame').attr('src', $('#mainFrame').attr('src'));
+    },
+    "ProtocolControl_Clicked":function () {
+        $("#mainFrame").attr("src", "protocolControl.html");
+        $('#mainFrame').attr('src', $('#mainFrame').attr('src'));
+    },
+    "About_Clicked":function () {
+        alert("空了写。。。");
+    }
+}
